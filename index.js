cac8382dbf7fb39433bb94c24940406a947e2e89
Mario Lubenka <mario.lubenka@googlemail.com>
2019-04-13 21:28:23 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+    process(options) {
+        if (!options.hasOwnProperty('inputFile')) {
+            throw Error('Missing option "inputFile".');
+        }
+        if (!options.hasOwnProperty('outputFile')) {
+            throw Error('Missing option "outputFile".');
+        }
+
+        var sassOptions = {
+            file: options['inputFile'],
+            outFile: options['outputFile'],
+            includePaths: options.hasOwnProperty('includePaths') ? options.hasOwnProperty('includePaths') : [],
+            importer: function(url) {
+                var sassTildeImporter = require('node-sass-tilde-importer');
+                return sassTildeImporter(url);
+            }
+        };
+
+        var sass = require('node-sass');
+        sass.render(sassOptions, function(err, result) {
+            console.log(result);
+        });
+    }
+};
