de93cc3a8ffac05d6c0166f53fc25d8443ab822a
Mario Lubenka <mario.lubenka@googlemail.com>
2022-11-04 12:19:49 +0100

    chore: update dev dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
 module.exports = {
-    globals: {
-        'ts-jest': {
-            tsConfig: 'tsconfig.json',
-        },
-    },
     moduleFileExtensions: [
         'js',
         'json',
         'ts',
     ],
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
+        '^.+\\.(ts|tsx)$': ['ts-jest', {
+            tsConfig: 'tsconfig.json',
+        }],
     },
     testMatch: [
         '**/test/**/*.test.ts',
